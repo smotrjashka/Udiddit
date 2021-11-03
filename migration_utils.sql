ae3c1d6775eac627490794fd4623d92e88353c00
Anastasiia Hrytskova <grickov130@ya.ru>
2021-11-03 17:19:48 +0200

    Update migration_utils.sql

--- migration_utils.sql
+++ migration_utils.sql
@@ -1,3 +1,7 @@
+
+select COUNT(*) from bad_posts where LENGTH(username)>25;
+select COUNT(*) from bad_comments WHERE LENGTH(username)>25;
+
 SELECT COUNT(*) FROM bad_posts WHERE LENGTH(TRIM(topic)) <1;
 
 SELECT DISTINCT(topic) FROM bad_posts WHERE LENGTH(topic)>30 OR LENGTH(TRIM(topic))<1;
