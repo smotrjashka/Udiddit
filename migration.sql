5786895468b5e768374498f1399a23c1aa112af9
Anastasiia Hrytskova <grickov130@ya.ru>
2021-11-03 17:14:29 +0200

    Update migration.sql

--- migration.sql
+++ migration.sql
@@ -6,3 +6,7 @@ INSERT INTO good_users (username)
 SELECT * FROM usernames; 
 ---we lest users who only votes for next time---
 
+
+INSERT INTO good_topics (topic_name) SELECT DISTINCT(topic) FROM bad_posts;
+
+SELECT count(*) FROM bad_posts WHERE url IS NOT NULL and text_content IS NOT NULL;
