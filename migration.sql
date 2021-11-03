99054c521d536e9299c793f34a11994123a8c50c
Anastasiia Hrytskova <grickov130@ya.ru>
2021-11-03 17:33:21 +0200

    migrate posts only

--- migration.sql
+++ migration.sql
@@ -9,4 +9,8 @@ SELECT * FROM usernames;
 
 INSERT INTO good_topics (topic_name) SELECT DISTINCT(topic) FROM bad_posts;
 
-SELECT count(*) FROM bad_posts WHERE url IS NOT NULL and text_content IS NOT NULL;
+INSERT INTO good_posts (title, topic_id, user_id, url, text_content)
+SELECT substring(bad_posts.title from 0 for 100), good_topics.id, good_users.id,  bad_posts.url, bad_posts.text_content 
+FROM bad_posts 
+JOIN good_users ON bad_posts.username=good_users.username
+JOIN good_topics ON bad_posts.topic=good_topics.topic_name;
